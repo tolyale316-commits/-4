606b3c0296ea93731f79985ea47225f9b50542c9
tolyale316-commits <tolya.le316@gmail.com>
2025-11-14 10:36:19 +0400

    коммит 2

---
+++ лаб 4 згуральская/лаб 4 згуральская/лаб 4 згуральская.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main() {
+    int N;
+
+    printf("Введите число N: ");
+    scanf("%d", &N);
+
+    for (int i = 1; i = N; i++)
+    {
+        for (int j = N; j > N - i; j--)
+        {
+            printf("%d, j");
+        }
+        printf("\n");
+        printf("Введите число N: ");
+    }
+    return 0;
+}
